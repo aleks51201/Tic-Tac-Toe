4f179e2c5c51d474ddee078f9a048b845acd285e
KipoGo <vit-tan@mail.ru>
2022-07-06 17:22:36 +0300

    Add new classes

---
+++ Assets/Scripts/test/CellBase.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class CellBase : MonoBehaviour
+{
+    public void SetColor()
+    {
+
+    }
+
+    public void SetTag()
+    {
+
+    }
+
+
+    
+}
--- Assets/Scripts/test/FieldBase.cs
+++ Assets/Scripts/test/FieldBase.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public  class Field : MonoBehaviour,IField
+public  abstract class FieldBase : MonoBehaviour,IField
 {
     public void Create()
     {
